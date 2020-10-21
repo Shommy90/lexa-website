cfd2097bd7909d27c336094033b474aceff7185e
Milovan <marjanovic90@gmail.com>
2020-10-21 17:34:03 +0200

    add team section

--- js/tabs.js
+++ js/tabs.js
@@ -1,38 +1,73 @@
 $(document).ready(function () {
-  // tabbed content
-  // http://www.entheosweb.com/tutorials/css/tabs.asp
   $(".tab__content").hide();
   $(".tab__content:first").show();
 
-  /* if in tab mode */
   $("ul.tabs li").click(function () {
-    console.log("click");
     $(".tab__content").hide();
     var activeTab = $(this).attr("rel");
     $("#" + activeTab).fadeIn();
 
     $("ul.tabs li").removeClass("active");
     $(this).addClass("active");
-
-    $(".tab_drawer_heading").removeClass("d_active");
-    $(".tab_drawer_heading[rel^='" + activeTab + "']").addClass("d_active");
   });
+});
 
-  /* if in drawer mode */
-  $(".tab_drawer_heading").click(function () {
-    $(".tab__content").hide();
-    var d_activeTab = $(this).attr("rel");
-    $("#" + d_activeTab).fadeIn();
+$(document).ready(function () {
+  $(".content--executive").hide();
+  $(".content--executive:first").show();
+  $("li.tab--executive:first").addClass("active");
 
-    $(".tab_drawer_heading").removeClass("d_active");
-    $(this).addClass("d_active");
+  $("li.tab--executive").click(function () {
+    $(".content--executive").hide();
+    var activeTab = $(this).attr("rel");
+    $("#" + activeTab).fadeIn();
 
-    $("ul.tabs li").removeClass("active");
-    $("ul.tabs li[rel^='" + d_activeTab + "']").addClass("active");
+    $("li.tab--executive").removeClass("active");
+    $(this).addClass("active");
   });
+});
 
-  /* Extra class "tab_last"
-      to add border to right side
-      of last tab */
-  $("ul.tabs li").last().addClass("tab_last");
+$(document).ready(function () {
+  $(".content--supervisory").hide();
+  $(".content--supervisory:first").show();
+  $("li.tab--supervisory:first").addClass("active");
+
+  $("li.tab--supervisory").click(function () {
+    $(".content--supervisory").hide();
+    var activeTab = $(this).attr("rel");
+    $("#" + activeTab).fadeIn();
+
+    $("li.tab--supervisory").removeClass("active");
+    $(this).addClass("active");
+  });
+});
+
+$(document).ready(function () {
+  $(".content--advisors").hide();
+  $(".content--advisors:first").show();
+  $("li.tab--advisors:first").addClass("active");
+
+  $("li.tab--advisors").click(function () {
+    $(".content--advisors").hide();
+    var activeTab = $(this).attr("rel");
+    $("#" + activeTab).fadeIn();
+
+    $("li.tab--advisors").removeClass("active");
+    $(this).addClass("active");
+  });
+});
+
+$(document).ready(function () {
+  $(".content--partners").hide();
+  $(".content--partners:first").show();
+  $("li.tab--partners:first").addClass("active");
+
+  $("li.tab--partners").click(function () {
+    $(".content--partners").hide();
+    var activeTab = $(this).attr("rel");
+    $("#" + activeTab).fadeIn();
+
+    $("li.tab--partners").removeClass("active");
+    $(this).addClass("active");
+  });
 });
